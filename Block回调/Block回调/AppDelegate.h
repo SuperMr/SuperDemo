4eb5de0fa56ab4f2a80595ecb38f5abaad71082c
super <super@zhangguanchaoMacBookPro.local>
2016-07-12 09:24:16 +0800

    第一次提交

---
+++ Block回调/Block回调/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Block回调
+//
+//  Created by super on 16/7/9.
+//  Copyright © 2016年 super. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Block回调/Block回调/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Block回调
+//
+//  Created by super on 16/7/9.
+//  Copyright © 2016年 super. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIButton *btnOutlet;
+- (IBAction)btnClick:(UIButton *)sender;
+
+
+@end
+
